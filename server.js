4ab2fda07959ed53cf6bac4bdb596f4fc8decb06
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-10-07 13:12:33 -0300

    aaa

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.get('/(:id)', function(req, res) {
     res.render('pad');
   });
 
+  let redisClient;
+  console.log(process.env.REDISTOGO_URL);
+  if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    redisClient = redis.createClient(rtg.port, rtg.hostname);
+    redisClient.auth(rtg.auth.split(":")[1]);
+  } else {
+    redisClient = redis.createClient();
+  }
+
+
 const options = {
     db: {type: 'redis'},
   };
